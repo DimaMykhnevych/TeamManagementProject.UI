9a53c993b5bb6f8b66640b2295fba182cd70ee3c
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-05-13 22:45:43 +0300

    Add material module and finish making company registration form (without validation)

--- FrontEnd/src/app/company-registration/company-registration.module.ts
+++ FrontEnd/src/app/company-registration/company-registration.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompanyRegistrationPageComponent } from './components/company-registration-page/company-registration-page.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MaterialModule } from '../layout';
 
 @NgModule({
   declarations: [CompanyRegistrationPageComponent],
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, MaterialModule],
 })
 export class CompanyRegistrationModule {}
--- FrontEnd/src/app/company-registration/components/company-registration-page/company-registration-page.component.ts
+++ FrontEnd/src/app/company-registration/components/company-registration-page/company-registration-page.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-company-registration-page',
   templateUrl: './company-registration-page.component.html',
-  styleUrls: ['./company-registration-page.component.css']
+  styleUrls: ['./company-registration-page.component.css'],
 })
 export class CompanyRegistrationPageComponent implements OnInit {
+  public form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _builder: FormBuilder) {
+    this.form = this._builder.group({});
   }
 
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  private initializeForm(): void {
+    this.form = this._builder.group({
+      companyName: new FormControl('', Validators.required),
+      domain: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+  get companyName() {
+    return this.form.get('companyName');
+  }
+  get domain() {
+    return this.form.get('domain');
+  }
+  get address() {
+    return this.form.get('address');
+  }
+  get email() {
+    return this.form.get('email');
+  }
+  get phone() {
+    return this.form.get('phone');
+  }
+  get password() {
+    return this.form.get('password');
+  }
 }
