67e7262d2c6e0a5bbb3d064640ab3ba21013ca48
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-05-17 01:02:36 +0300

    Start using Stripe API

--- FrontEnd/src/app/company-registration/components/subscription-payment/subscription-payment.component.ts
+++ FrontEnd/src/app/company-registration/components/subscription-payment/subscription-payment.component.ts
@@ -1,5 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,7 +15,12 @@ import { Router } from '@angular/router';
 })
 export class SubscriptionPaymentComponent implements OnInit {
   public form: FormGroup;
-  constructor(private _router: Router, private _builder: FormBuilder) {
+  takePaymentResult: string;
+  constructor(
+    private _router: Router,
+    private _builder: FormBuilder,
+    private _http: HttpClient
+  ) {
     this.form = this._builder.group({});
   }
 
@@ -17,8 +28,65 @@ export class SubscriptionPaymentComponent implements OnInit {
     this.initializeForm();
   }
 
+  takePayment(productName: string, amount: number, token: any) {
+    let body = {
+      tokenId: token.id,
+      productName: productName,
+      amount: amount,
+    };
+    let bodyString = JSON.stringify(body);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    //let options = new RequestOptions({ headers: headers });
+
+    // this._http
+    //   .post("/api/stripepayment", bodyString, options)
+    //   .toPromise()
+    //   .then(res => {
+    //     this.takePaymentResult = res.json().status;
+    //   })
+    //   .catch(error => {
+    //     this.takePaymentResult = error.message;
+    //   });
+  }
+
+  openCheckout(productName: string, amount: number, tokenCallback) {
+    let handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51Irp6yF27ICkRRw9XrC5XUCd43fCvV4YGyhyg1nEnYjY3dr8Iegpjo0o0dviTYptZ4Unoaaek4X3vCnNFTmtZtGa00GmwLCiKT',
+      locale: 'auto',
+      token: tokenCallback,
+    });
+
+    handler.open({
+      name: 'Our Shop',
+      description: productName,
+      zipCode: false,
+      currency: 'gbp',
+      amount: amount,
+      panelLabel: 'Pay {{amount}}',
+      allowRememberMe: false,
+    });
+  }
+
+  buyTShirt() {
+    this.openCheckout('T-Shirt', 1000, (token: any) =>
+      this.takePayment('T-Shirt', 1000, token)
+    );
+  }
+  buyTrainers() {
+    this.openCheckout('Trainers', 1500, (token: any) =>
+      this.takePayment('Trainers', 1500, token)
+    );
+  }
+  buyJeans() {
+    this.openCheckout('Jeans', 2000, (token: any) =>
+      this.takePayment('Jeans', 2000, token)
+    );
+  }
+
   private initializeForm(): void {
-    this.form = this._builder.group({});
+    this.form = this._builder.group({
+      name: new FormControl('', [Validators.required]),
+    });
   }
 
   public onBackToHomeClick(): void {
